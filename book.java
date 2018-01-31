6bfe9689008348bd4a6bb9ea4e6ae9144ee1ebea
luojiong <luojion11@qq.com>
2018-01-31 16:11:03 +0800

    修改bugger

--- book.java
+++ book.java
@@ -2,9 +2,11 @@
 //fewfoeiwjfoi
 public class book{
 public static void main(Stirng[] args){
+	String age="sfreogjrioegjioer";
 	System.out.println("sfheuw");
+	age=getInfr(age);
 }
 }
-public String getInfr(String age){
+public static String getInfr(String age){
 	return age.substring(0,age.length-2);
 }
